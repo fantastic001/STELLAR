4a607bca71e0e897191cdeed969472bf918b7684
Stefan Nožinić <stefan@lugons.org>
2025-04-01 12:33:39 +0200

    Update usage message in stellar/__main__.py

--- stellar/__main__.py
+++ stellar/__main__.py
@@ -6,7 +6,7 @@ sys.setrecursionlimit(64*1024)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Usage: python -m table_transformer <path_to_file>")
+        print("Usage: python -m stellar <path_to_file>")
         sys.exit(1)
     f = open(sys.argv[1], "r")
     run(f.read())
