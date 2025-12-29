d735324ffa5daaf9bd060357c9a2dff095e6544e
Stefan Nožinić <stefan@lugons.org>
2025-12-29 13:24:03 +0400

    Update requirements and refactor main module to integrate command execution and LSP functionality

--- stellar/__main__.py
+++ stellar/__main__.py
@@ -1,14 +1,22 @@
 from stellar import *
+from stellar.server import server
 import sys 
+from orgasm import command_executor_main
+
+class Commands:
+    def run(self, path: str):
+        with open(path, "r") as f:
+            run(f.read())
+    def lsp_tcp(self, port: int, *, host: str = "127.0.0.1"):
+        server.start_tcp(host, port)
+    
+    def lsp_stdio(self):
+        server.start_io()
 
 # set maximum recursion depth 
 def main():
     sys.setrecursionlimit(64*1024)
-    if len(sys.argv) < 2:
-        print("Usage: python -m stellar <path_to_file>")
-        sys.exit(1)
-    f = open(sys.argv[1], "r")
-    run(f.read())
+    command_executor_main(Commands, explicit_params=False)
 if __name__ == "__main__":
     main()
     
